bac771780a8aaa2c2ce6a8de495d1db658cdfa46
najimashaikh <najimashaikh267@gmail.com>
2026-09-01 11:49:51 +0530

    feat: implement root layout with global providers, SEO metadata, and marketing tracking scripts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -128,6 +128,13 @@ export default function RootLayout({
         <link rel="dns-prefetch" href="https://supabase.co" />
         <link rel="preconnect" href="https://images.unsplash.com" crossOrigin="anonymous" />
         <link rel="dns-prefetch" href="https://images.unsplash.com" />
+        {/* Impact.com Affiliate Tracking & Website Verification Script */}
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `(function(i,m,p,a,c,t){c.ire_o=p;c[p]=c[p]||function(){(c[p].a=c[p].a||[]).push(arguments)};t=a.createElement(m);var z=a.getElementsByTagName(m)[0];t.async=1;t.src=i;z.parentNode.insertBefore(t,z)})('https://utt.impactcdn.com/P-A7711863-ac02-45ed-a75d-49aa37eb071a1.js','script','impactStat',document,window);impactStat('transformLinks');impactStat('trackImpression');`,
+          }}
+        />
       </head>
       <body className="bg-[#121212] text-white min-h-screen flex flex-col font-sans antialiased">
         <StoreOrganizationJsonLd />
